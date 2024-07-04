3cfa77b9e5d618878d94b18ddfbbafee70f14150
muzamil ahmed <amuzammil707@gmail.com>
2024-07-04 17:14:17 +0500

    Adding a Todo_List

---
+++ main.js
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+let todoList = [];
+let conditions = true;
+console.log("\n \t  Welcome to Muzamil Ahmed - Todolist Application\n");
+while (conditions) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: "Enter your New task:",
+        }
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask.task} Task Added in Todo-list successfuly`);
+    let addMoretask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want to add more task ?",
+        }
+    ]);
+    conditions = addMoretask.addmore;
+}
+console.log(`you updated Todo-list :`);
